9468c592b771e15ede7ffbbb9fe55923e27c228a
Tole15 <129608820+Tole15@users.noreply.github.com>
2023-04-01 13:21:50 -0600

    Lista_de_Puntos_with_r_equal_2
    This alternative version of the point list is generated for a circle with a radius of 2. This allows the figure's area to fit within the 50 cm^2 limit of the robot's workspace. Therefore, the figure can be drawn more accurately, avoiding any possible overflow problems in the workspace. With this optimized point list, better task efficiency and higher quality in the final drawing result are guaranteed.

---
+++ listadepuntosradio2.py
@@ -0,0 +1,29 @@
+import csv
+import math
+import os
+# definir el centro de la circunferencia
+centro_x = 0
+centro_y = 0
+
+# definir el radio de la circunferencia
+radio = 2
+
+# definir la cantidad de puntos a generar
+num_puntos = 50
+
+# generar lista de puntos
+puntos = []
+for i in range(num_puntos):
+    angulo = 2 * math.pi * i / num_puntos
+    x = centro_x + radio * math.cos(angulo)
+    y = centro_y + radio * math.sin(angulo)
+    puntos.append((x, y))
+
+# imprimir lista de puntos en formato CSV
+with open('puntos.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(['x', 'y'])
+    for punto in puntos:
+        writer.writerow(punto)
+print("¡Listo, en la dirección de abajo se  guardo el archivo!")
+print(os.getcwd())
